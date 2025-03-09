21a1fe7dc4f519fa304a4314080e3ab482d0c72b
melmanss <cfhfyjdbx@gmail.com>
2025-03-09 08:42:42 +0200

    Fahrenheit to Celsius conversion

--- src/app/TemperatureConverter.java
+++ src/app/TemperatureConverter.java
@@ -1,8 +1,24 @@
 package app;
 
+import java.util.Scanner;
+
 public class TemperatureConverter {
     public static void main(String[] args) {
-
         System.out.println("App for temperature converting");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть температуру в фаренгейтах: ");
+        double fahrenheit = scanner.nextDouble();
+
+        double celsius = fahrenheitToCelsius(fahrenheit);
+
+        System.out.printf("Температура в цельсіях: %.2f°C%n", celsius);
+
+        scanner.close();
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
     }
 }
