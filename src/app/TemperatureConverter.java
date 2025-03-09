d349c5147fa4c3ed3bf663412ef0b5ccf04d2eaa
melmanss <cfhfyjdbx@gmail.com>
2025-03-09 09:25:00 +0200

    Celsius to Fahrenheit conversion

--- src/app/TemperatureConverter.java
+++ src/app/TemperatureConverter.java
@@ -8,17 +8,35 @@ public class TemperatureConverter {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введіть температуру в фаренгейтах: ");
-        double fahrenheit = scanner.nextDouble();
+        System.out.print("Введіть '1' для конвертації з фаренгейтів в цельсії або '2' для конвертації з цельсіїв в фаренгейти: ");
+        int choice = scanner.nextInt();
 
-        double celsius = fahrenheitToCelsius(fahrenheit);
+        if (choice == 1) {
 
-        System.out.printf("Температура в цельсіях: %.2f°C%n", celsius);
+            System.out.print("Введіть температуру в фаренгейтах: ");
+            double fahrenheit = scanner.nextDouble();
+            double celsius = fahrenheitToCelsius(fahrenheit);
+            System.out.printf("Температура в фаренгейтах: %.2f°F%n", fahrenheit);
+            System.out.printf("Температура в цельсіях: %.2f°C%n", celsius);
+        } else if (choice == 2) {
+
+            System.out.print("Введіть температуру в цельсіях: ");
+            double celsius = scanner.nextDouble();
+            double fahrenheit = celsiusToFahrenheit(celsius);
+            System.out.printf("Температура в цельсіях: %.2f°C%n", celsius);
+            System.out.printf("Температура в фаренгейтах: %.2f°F%n", fahrenheit);
+        } else {
+            System.out.println("Неправильний вибір. Будь ласка, спробуйте ще раз.");
+        }
 
         scanner.close();
     }
 
     public static double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
     }
 }
